89f8376e235703ef812ac6b166141b1b76996dca
近村修斗 <scmarudekaze@gmail.com>
2022-10-01 16:39:18 +0900

    develop

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,6 +16,7 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        
 
         <p className={styles.description}>
           Get started by editing{' '}
